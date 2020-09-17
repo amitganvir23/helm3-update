ff2a1630f1067649787a43615b460217ccb54454
Open Source book <amitganvir6@gmail.com>
2020-09-18 00:49:17 +0530

    Update before-deploy-start.sh

--- hello2-java-app-configmap-volume/before-deploy-start.sh
+++ hello2-java-app-configmap-volume/before-deploy-start.sh
@@ -1,4 +1,4 @@
-kubectl -n glt-dev create configmap english-hindi --from-file=./configmapfiles/english/
+kubectl -n glt-dev create configmap hindi-configmap --from-file=./configmapfiles/hindi/
 kubectl -n glt-dev create configmap english-configmap --from-file=./configmapfiles/english/
 kubectl -n glt-dev get configmap
 kubectl -n glt-dev create -f deploy.yml -f service.yml
